2db8b8f1e5ba77f8fdb74e9fce81e9a5d05bce59
Nino Paolo <o70vd@yahoo.com>
2013-03-04 00:40:09 +0800

    Added test

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,14 +9,16 @@ class codeigniter (
   exec { 'download-codeigniter':
     cwd     => '/tmp',
     command => '/usr/bin/wget http://ellislab.com/codeigniter/download',
-    creates => '/tmp/download'
+    creates => '/tmp/download',
+    unless  => "/usr/bin/test -d ${root}/system/core"
   }
 
   
   exec { 'unzip-codeigniter':
     cwd     => '/tmp',
     command => "/usr/bin/unzip download && /bin/cp -R CodeIgniter_*/* ${root} && /bin/rm -rf download Codeigniter_*",
-    require => [ File["${root}"], Package['unzip'], Exec['download-codeigniter'] ]
+    require => [ File["${root}"], Package['unzip'], Exec['download-codeigniter'] ],
+    unless  => "/usr/bin/test -d ${root}/system/core"
   }
 
   # http://getsparks.org/install
@@ -24,6 +26,6 @@ class codeigniter (
     cwd     => "${root}",
     command => '/usr/bin/php -r "$(curl -fsSL http://getsparks.org/go-sparks)"',
     require => [ Package['php5-cli'], File["${root}"], ],
+    unless  => "/usr/bin/test -e ${root}/tools/spark"
   }
-
 }
